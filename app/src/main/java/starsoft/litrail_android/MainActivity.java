779d8ffb1baeeba765bdc56a7f3dbefb15f66e15
Adomas <31891228+ramumnesys@users.noreply.github.com>
2017-09-24 15:19:19 +0300

    Timetable UI (#5)
    * Implement timetable UI
    
    * Implement responsive UI 
    
    * Customise timetable UI layout
    
    * Add listeners

--- app/src/main/java/starsoft/litrail_android/MainActivity.java
+++ app/src/main/java/starsoft/litrail_android/MainActivity.java
@@ -1,17 +1,37 @@
 package starsoft.litrail_android;
 
+import android.content.DialogInterface;
+import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.graphics.PorterDuff;
+import android.icu.text.DateFormat;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.DatePicker;
+import android.widget.ListAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
-
-    private TextView mTextMessage;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -20,16 +40,12 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
                     return true;
                 case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
                     return true;
                 case R.id.navigation_map:
-                    mTextMessage.setText(R.string.title_map);
                     return true;
                 case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
                     return true;
             }
             return false;
@@ -41,21 +57,74 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle("Tvarkaraščių užklausa");
 
-        mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        final Button departureStationButton = (Button) findViewById(R.id.buttonDeparture);
+        final Button arrivalStationButton = (Button) findViewById(R.id.buttonDestination);
+        final Button searchButton = (Button) findViewById(R.id.buttonSearch);
+        final CalendarView departureDatePicker = (CalendarView) findViewById(R.id.calendarView);
+
+        final Calendar departureDate = Calendar.getInstance();
 
         // rodomi navigacijos elementų pavadinimai
         BottomNavigationViewExpander.disableShiftMode(navigation);
 
+        //demo DB stočių duomenys
+        List<String> stations = new ArrayList<>();
+        stations.add("Vilnius");
+        stations.add("Kaunas");
+        stations.add("Šiauliai");
+        stations.add("Klaipėda");
+        stations.add("Panevėžys");
+        stations.add("Radviliškis");
+        final ArrayAdapter<String> stationsAdapter =
+                new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, stations);
 
+        // paspaustas išvykimo stočių mygtukas, rodantis sąrašą vietų
+        departureStationButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                departureStationButton.getBackground().setColorFilter(ContextCompat.getColor(getApplicationContext(), R.color.text), PorterDuff.Mode.SRC_ATOP);
+                popUpLocationPicker(getResources().getString(R.string.select_departure_station), stationsAdapter, departureStationButton);
+            }
+        });
+        // paspaustas atvykimo stočių mygtukas, rodantis sąrašą vietų
+        arrivalStationButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                arrivalStationButton.getBackground().setColorFilter(ContextCompat.getColor(getApplicationContext(), R.color.text), PorterDuff.Mode.SRC_ATOP);
+                popUpLocationPicker(getResources().getString(R.string.select_arrival_station), stationsAdapter, arrivalStationButton);
+            }
+        });
 
+        // paspaustas paieškos mygtukas, pateikiantis paieškos užklausą kontroleriui
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if (!isInputValid(departureStationButton, arrivalStationButton)) {
+                    Toast.makeText(getApplicationContext(), "Klaida. Nenurodytas pilnas maršrutas.", Toast.LENGTH_SHORT).show();
+                }
+                else if (departureStationButton.getText().equals(arrivalStationButton.getText())) {
+                    Toast.makeText(getApplicationContext(), "Tokiam maršrutui traukinių nereikia!", Toast.LENGTH_SHORT).show();
+                }
+                else Toast.makeText(getApplicationContext(), "Ok", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        // Pakeista data datos pasirinkimo widget'e
+        departureDatePicker.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                departureDate.set(year, month, dayOfMonth);
+                Toast.makeText(getApplicationContext(),
+                        "Data: " + year + " " + month + " " + dayOfMonth, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
+    // Inicializuojamas viršutinis meniu langas
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -63,6 +132,7 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
+    // paspaustas mygtukas viršutiniame meniu lange
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
@@ -79,4 +149,32 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    private void popUpLocationPicker(String title, final ArrayAdapter<String> adapter, final Button button) {
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
+        dialogBuilder.setTitle(title);
+        dialogBuilder.setAdapter(adapter, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int item) {
+                // pakeičia mygtuko tekstą į pasirinktą
+                button.setText(adapter.getItem(item));
+
+            }
+        });
+        AlertDialog alertDialogObject = dialogBuilder.create();
+        alertDialogObject.show();
+    }
+
+    private boolean isInputValid(Button button1, Button button2) {
+        boolean status = true;
+
+        if (button1.getText().equals(getResources().getString(R.string.undefined_place))) {
+            status = false;
+            button1.getBackground().setColorFilter(ContextCompat.getColor(getApplicationContext(), R.color.error), PorterDuff.Mode.SRC_ATOP);
+        }
+        if (button2.getText().equals(getResources().getString(R.string.undefined_place))) {
+            status = false;
+            button2.getBackground().setColorFilter(ContextCompat.getColor(getApplicationContext(), R.color.error), PorterDuff.Mode.SRC_ATOP);
+        }
+
+        return status;
+    }
 }
